e004dada1844fec58a10e8dc29e1dfe38e915972
pushpraj15295 <pushpraj15295@gmail.com>
2025-07-19 11:54:21 +0530

    type and variable

---
+++ three.py
@@ -0,0 +1,15 @@
+# variable and type 
+
+a = 12
+b = "new str"
+c = 1.1
+
+
+# x = a + b 
+#diffrent type opration is not possiabel in python
+
+
+# types
+print(type(a),type(b))
+
+print("type of c is", type(c))
