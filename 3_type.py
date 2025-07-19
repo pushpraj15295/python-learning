ee6795482f62aed083a8d9bf8a1ed5a80fff0831
pushpraj15295 <pushpraj15295@gmail.com>
2025-07-19 13:21:30 +0530

    input value for oprations

--- 3_type.py
+++ 3_type.py
@@ -8,6 +8,14 @@ lists = [1,3, "ray" , ["hi" , a]]  # nothing but array
 
 dict1 = { "f":1, "s" :"second", "t":True} # nothing but object 
 
+
+# multi line string
+str = """hey i am
+   
+   jon
+   
+"""
+
 # x = a + b 
 #diffrent type opration is not possiabel in python
 
@@ -19,3 +27,7 @@ print("type of c is", type(c))
 
 print(type(lists))
 print(type(dict1))
+
+print(str)
+
+print(str[1])
---
+++ 6_input.py
@@ -0,0 +1,13 @@
+# input value is always in str
+
+val = input("type here...")
+
+print("you have enter -", val)
+
+# add 2 input 
+
+a = input("enter first number")
+b = input("second first number")
+
+
+print("total is - ",int(a) + int(b))
