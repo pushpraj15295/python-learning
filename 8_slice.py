c365a84139c0b5ee46201ed0b34e4a9efab828b5
pushpraj15295 <pushpraj15295@gmail.com>
2025-07-19 13:45:06 +0530

    slice the string

---
+++ 8_slice.py
@@ -0,0 +1,13 @@
+x = "may___name_is-raj"
+
+print("from start -",x[0:5])
+
+# or without zero also same output for firt place it will put 0 and for last place it will put length of str
+print("from start -",x[:5])
+
+
+print("from any place -",x[3:9])
+
+# accordin to length
+
+print("from any to last", x[5 :len(x)-1])
