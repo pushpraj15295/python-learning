96cdf6d6495c486bd0969d6b6f6c09c0c49ac8c1
pushpraj15295 <pushpraj15295@gmail.com>
2025-07-19 11:36:41 +0530

    printing part

---
+++ two.py
@@ -0,0 +1,7 @@
+# for sapreator
+print("hii",4, 6 , sep="$", end="xyz")
+
+# print will gave in same line for new line print \n
+print("hii",86, 61 , sep="_", end="xyz\n")
+print("hii",4, 6 , sep="~")
+
