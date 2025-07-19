dbba23f4a62d363623a272c75b9adb816f1d1e7f
pushpraj15295 <pushpraj15295@gmail.com>
2025-07-19 17:50:54 +0530

    methord for string

---
+++ 9_str_methord.py
@@ -0,0 +1,34 @@
+# str are immutables 
+
+str = "pushPraj patel"
+
+print(len(str))
+
+print(str.upper())
+print(str.lower())
+print(str.rstrip("l")) # remove last
+
+print(str.replace("pu","si"))
+
+print(str.split(" "))
+
+print(str.capitalize()) #first only
+print(str.title())  # all 
+
+print(str.center(20))
+
+print(str.count("pu"))
+
+print(str.endswith("j"))
+
+# from index to index
+print(str.endswith("el",5,45))
+
+print(str.find("aj"))
+
+print(str.islower())
+
+
+sp = "  "
+print(sp.isspace())
+
