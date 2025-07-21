9a432e0b9c14ed8df79438fb0227f6397533c49c
pushpraj15295 <pushpraj15295@gmail.com>
2025-07-21 22:54:25 +0530

    while loops

--- 7_loops.py
+++ 7_loops.py
@@ -9,3 +9,38 @@ num = "239889"
 
 for i in num:
     print(int(i)+1)
+    
+    
+# for looping numbers range 
+for k in range(50):
+  print(k+1)
+
+# till before last point (n-1)
+for k in range(5,11):
+  print(k+1)
+  
+# with step  till (n-1)
+for k in range(5,15,3):
+  print(k)
+  
+  
+  
+  
+#  while loops 
+
+y = 2
+
+while( y <= 10):
+  print(y)
+  y = y+2
+  
+#  conditional 
+
+val = int(input("enter your number - "))
+
+while(val < 50):
+  val = int(input("enter your number - "))
+  print(val)
+else : print("you done")
+  
+print("You have passed the level")
