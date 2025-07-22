5522597bca4cbbc55693e5b3d3883a045b790659
pushpraj15295 <pushpraj15295@gmail.com>
2025-07-22 22:18:50 +0530

    funtion def

---
+++ 13_function.py
@@ -0,0 +1,14 @@
+def cal(a,b):
+    val = a+b
+    print("sum is a and b is - ",val)
+    if(a>b):
+        print("a is greater")
+    else:
+        print("b is greater")
+    
+    
+x = 4
+y = 5
+
+cal(x,y)   
+    
