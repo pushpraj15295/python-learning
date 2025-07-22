e99409c0a01849d55591f756c9dafe3fc2a38f44
pushpraj15295 <pushpraj15295@gmail.com>
2025-07-22 22:09:05 +0530

    break and continueu

---
+++ 12_break_continue.py
@@ -0,0 +1,13 @@
+# break
+
+for i in range(12):
+    if(i == 10):break
+    print("5 X", i+1 ,"=" ,5*(i+1))
+    
+# continue
+
+for i in range(7):
+  if(i == 4):
+      print("i skiped 4")
+      continue
+  print(i)
