7d7baf2eae0d8cbcc79f124184839ce081f0414d
pushpraj15295 <pushpraj15295@gmail.com>
2025-07-22 22:38:05 +0530

    arguments in functions

--- 13_function.py
+++ 13_function.py
@@ -12,3 +12,23 @@ y = 5
 
 cal(x,y)   
     
+# default arguments in def funtion
+
+def multiply(x = 2, y= 3):
+    print("multi of x and y is - ", x *y)
+
+multiply(3,5)
+multiply()
+multiply(4)
+multiply(y=7)
+multiply(y=7,x=5)
+
+# number tuple - group of number
+def avg(*num):
+    print(type(num))
+    sum = 0
+    for i in num:
+        sum = sum+i
+    return sum/len(num)
+        
+print(avg(3,4,5,6,7,8))
