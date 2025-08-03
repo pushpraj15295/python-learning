5b23afcc80c66c426d0c49872b76c053a6b9e0ca
pushpraj15295 <pushpraj15295@gmail.com>
2025-08-03 12:13:31 +0530

    sort hand if else

---
+++ 29_sorthand_if_else.py
@@ -0,0 +1,8 @@
+# 
+
+a = 20
+b = int(input("enter num - "))
+
+
+print("greater") if a < b else print("same value") if a == b else print("smaller")
+# print kro greater agr a< b se ,nahito print "same valse" agr a==b ,nahito print "samller"
