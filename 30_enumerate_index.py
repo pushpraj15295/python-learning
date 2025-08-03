43ad251788d373fae4b12431c0201d41a1caeffc
pushpraj15295 <pushpraj15295@gmail.com>
2025-08-03 15:12:11 +0530

    enumerate for index

---
+++ 30_enumerate_index.py
@@ -0,0 +1,9 @@
+nums = [1,3,4,6,7,9,7,5,4,3,2]
+
+# for index 
+# for index, num in enumerate(nums , start = 1):
+# or
+for index, num in enumerate(nums):
+      print(index, "--- and num is", num)
+      if(index == 8): print("you got the index 8")
+      
